37dac4ffcf322bfd29caefde01e95ef19ea53a8d
baileynance <baieyj.nance@gmail.com>
2024-06-15 21:50:36 -0700

    part 5 done

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+
+const mongodb = require("./data/database");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use("/", require("./routes"));
 
-app.listen(port, () => {
-    console.log(`App is listening on port ${port}`);
-})
+mongodb.initDB((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Database is listening and node app is running on port ${port}`);
+        })
+    }
+});
